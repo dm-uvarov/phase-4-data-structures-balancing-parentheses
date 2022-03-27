3da94af34a4103d204d1bc89e275bdde4aa84151
Jimmy <dm.a.uvarov@gmail.com>
2022-03-27 14:39:48 -0400

    commit tree d816cee31a13e84a977bd5b5cf38522e5c539cb9

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,14 @@
 require_relative './stack'
 
 # your code here
+
+def balancing_parentheses(str)
+    stack_to_count   = Stack.new
+
+    (str.split("").filter{ |e|  (e=="(")}.size - str.split("").filter{ |e|  (e==")")}.size).abs.times do 
+        stack_to_count.push("*")
+    end
+    
+    stack_to_count.size
+
+end
